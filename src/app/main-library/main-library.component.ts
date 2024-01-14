f65661697566d4e1d998a664eedee3e6d058a765
biancaOnofrei-04 <bianca-ioana.onofrei@student.tuiasi.ro>
2024-01-14 14:20:26 +0200

    Main components

---
+++ src/app/main-library/main-library.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-library',
+  standalone: true,
+  imports: [],
+  templateUrl: './main-library.component.html',
+  styleUrl: './main-library.component.css'
+})
+export class MainLibraryComponent {
+
+}
---
+++ src/app/personal-library/personal-library.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-personal-library',
+  standalone: true,
+  imports: [],
+  templateUrl: './personal-library.component.html',
+  styleUrl: './personal-library.component.css'
+})
+export class PersonalLibraryComponent {
+
+}
