656cbce993d84723d788b464b7619be51fa22e84
biancaOnofrei-04 <bianca-ioana.onofrei@student.tuiasi.ro>
2024-01-14 18:30:04 +0200

    Made navbar fill whole page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet ,RouterModule} from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, 
+    RouterOutlet,
+    RouterModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+    MatListModule,
+    MatToolbarModule,
+  MatSidenavModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-library';
+  showFiller=false;
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
-import { Routes } from '@angular/router';
+import { Routes,RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+import { PersonalLibraryComponent } from './personal-library/personal-library.component';
+import { MainLibraryComponent } from './main-library/main-library.component';
+import { AddBooksComponent } from './add-books/add-books.component';
+
+export const routes: Routes = [
+    { path: '', redirectTo: '/main-library', pathMatch: 'full' }, // Default route
+    { path: 'personal-library', component: PersonalLibraryComponent, title:"Personal Library" },
+    { path: 'main-library', component: MainLibraryComponent , title:"Main Library"},
+    { path: 'add-books', component: AddBooksComponent, title: "Add A New Book" },
+    { path: '**', redirectTo: '/main-library' }, //wildcard, redirect to main lib in case a url not found above is entered
+
+  ];
+  @NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
+  })
+  export class AppRoutingModule {}
